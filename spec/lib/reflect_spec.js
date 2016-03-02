5b98812f13e60e204439cd3f047ed277e2eeccec
Vinson Chuong <vinsonchuong@gmail.com>
2016-03-02 13:27:37 -0800

    Make reflect.specFiles take jasmine.json into account

--- spec/lib/reflect_spec.js
+++ spec/lib/reflect_spec.js
@@ -4,8 +4,13 @@ import * as fse from 'fs-extra-promise-es6';
 import {specFiles} from 'jasmine-es6/lib/reflect';
 
 describe('reflect', () => {
+  beforeEach(async () => {
+    await fse.mkdirs(path.resolve('spec/support'));
+  });
+
   afterEach(async () => {
     await fse.remove(path.resolve('project'));
+    await fse.remove(path.resolve('spec/support'));
   });
 
   describe('#specFiles', () => {
@@ -13,6 +18,23 @@ describe('reflect', () => {
       const {stdout} = await childProcess.exec(
         `find '${path.resolve('spec')}' -type f`);
       const expectedFiles = stdout.trim().split('\n');
+
+      const actualFiles = await specFiles();
+
+      expect(actualFiles.sort()).toEqual(expectedFiles.sort());
+    });
+
+    it('respects jasmine.json configuration', async () => {
+      await fse.writeJson(path.resolve('spec/support/jasmine.json'), {
+        spec_dir: 'spec',
+        spec_files: [
+          'lib/reflect_*.js'
+        ]
+      });
+
+      const expectedFiles = [
+        path.resolve('spec/lib/reflect_spec.js')
+      ];
 
       const actualFiles = await specFiles();
 
--- src/lib/reflect.js
+++ src/lib/reflect.js
@@ -1,7 +1,22 @@
 import * as path from 'path';
+import * as fse from 'fs-extra-promise-es6';
 import glob from 'glob';
 import {promisify} from 'node-promise-es6';
 
 export async function specFiles() {
-  return await promisify(glob)(path.resolve('spec', '**/*_spec.js'));
+  let jasmineJson;
+  try {
+    jasmineJson = await fse.readJson('spec/support/jasmine.json');
+  } catch (error) {
+    jasmineJson = await fse.readJson(
+      require.resolve('jasmine-es6/config/jasmine.json'));
+  }
+
+  const files = [];
+  for (const fileGlob of jasmineJson.spec_files) {
+    const globResults = await promisify(glob)(
+      path.resolve(jasmineJson.spec_dir, fileGlob));
+    files.push(...globResults);
+  }
+  return files;
 }
