9d4a6bb4da30bd239940cb55c9c4b2dc99cea5db
Vinson Chuong <vinsonchuong@gmail.com>
2016-03-03 13:34:46 -0800

    Make reflect spec robust to pass in npm-integration

--- spec/lib/reflect_spec.js
+++ spec/lib/reflect_spec.js
@@ -1,99 +1,139 @@
 import * as path from 'path';
-import {childProcess} from 'node-promise-es6';
+import {childProcess, fs} from 'node-promise-es6';
 import * as fse from 'fs-extra-promise-es6';
-import {configPath, config, specFiles} from 'jasmine-es6/lib/reflect';
 
-describe('reflect', () => {
-  describe('#configPath', () => {
-    describe('when the project does not provide a jasmine.json', () => {
-      it('returns the default config', async () => {
-        expect(await configPath())
-          .toBe(require.resolve('jasmine-es6/config/jasmine.json'));
-      });
+class Project {
+  path(...args) {
+    return path.resolve('project', ...args);
+  }
+
+  async write(files) {
+    for (const filePath of Object.keys(files)) {
+      const absolutePath = this.path(filePath);
+      const fileContents = files[filePath];
+      await fse.ensureFile(absolutePath);
+      if (typeof fileContents === 'object') {
+        await fse.writeJson(absolutePath, fileContents);
+      } else {
+        await fs.writeFile(absolutePath, fileContents);
+      }
+    }
+  }
+
+  async run(code) {
+    await this.write({
+      'run.js': "require('dist-es6/lib/run')(require('path').resolve('./code'))",
+      'code.js': code
     });
 
-    describe('when the project provides a jasmine.json', () => {
-      beforeEach(async () => {
-        await fse.mkdirs(path.resolve('spec/support'));
-        await fse.writeJson(path.resolve('spec/support/jasmine.json'), {
+    const {stdout: output} = await childProcess.exec(
+      'node run.js',
+      {
+        cwd: this.path()
+      }
+    );
+    return output.trim();
+  }
+}
+
+describe('reflect', () => {
+  afterEach(async () => {
+    await fse.remove(path.resolve('project'));
+  });
+
+  describe('#configPath', () => {
+    it('returns the default config when the project does not provide a jasmine.json', async () => {
+      const project = new Project();
+      const result = await project.run(`
+        import {configPath} from 'jasmine-es6/lib/reflect';
+        async function run() {
+          process.stdout.write(await configPath());
+        }
+        run();
+      `);
+
+      expect(result).toBe(require.resolve('jasmine-es6/config/jasmine.json'));
+    });
+
+    it('returns the provided config when the project provides a jasmine.json', async () => {
+      const project = new Project();
+      await project.write({
+        'spec/support/jasmine.json': {
           spec_dir: 'spec',
           spec_files: ['**/*_spec.js']
-        });
+        }
       });
-      afterEach(async () => {
-        await fse.remove(path.resolve('spec/support'));
-      });
+      const result = await project.run(`
+        import {configPath} from 'jasmine-es6/lib/reflect';
+        async function run() {
+          process.stdout.write(await configPath());
+        }
+        run();
+      `);
 
-      it('returns the provided config', async () => {
-        expect(await configPath())
-          .toBe(path.resolve('spec/support/jasmine.json'));
-      });
+      expect(result).toBe(project.path('spec/support/jasmine.json'));
     });
   });
 
   describe('#config', () => {
-    describe('when the project does not provide a jasmine.json', () => {
-      it('returns the default config', async () => {
-        expect(await config()).toEqual(await fse.readJson(require.resolve('jasmine-es6/config/jasmine.json')));
-      });
+    it('returns the default config when the project does not provide a jasmine.json', async () => {
+      const project = new Project();
+      const result = await project.run(`
+        import {config} from 'jasmine-es6/lib/reflect';
+        async function run() {
+          process.stdout.write(JSON.stringify(await config()));
+        }
+        run();
+      `);
+      expect(JSON.parse(result))
+        .toEqual(await fse.readJson(require.resolve('jasmine-es6/config/jasmine.json')));
     });
 
-    describe('when the project provides a jasmine.json', () => {
-      beforeEach(async () => {
-        await fse.mkdirs(path.resolve('spec/support'));
-        await fse.writeJson(path.resolve('spec/support/jasmine.json'), {
+    it('returns the provided config', async () => {
+      const project = new Project();
+      await project.write({
+        'spec/support/jasmine.json': {
           spec_dir: 'spec',
           spec_files: ['**/*_spec.js']
-        });
+        }
       });
-      afterEach(async () => {
-        await fse.remove(path.resolve('spec/support'));
-      });
-
-      it('returns the provided config', async () => {
-        expect(await config()).toEqual({
-          spec_dir: 'spec',
-          spec_files: ['**/*_spec.js']
-        });
+      const result = await project.run(`
+        import {config} from 'jasmine-es6/lib/reflect';
+        async function run() {
+          process.stdout.write(JSON.stringify(await config()));
+        }
+        run();
+      `);
+      expect(JSON.parse(result)).toEqual({
+        spec_dir: 'spec',
+        spec_files: ['**/*_spec.js']
       });
     });
   });
 
   describe('#specFiles', () => {
-    beforeEach(async () => {
-      await fse.mkdirs(path.resolve('spec/support'));
-    });
-
-    afterEach(async () => {
-      await fse.remove(path.resolve('project'));
-      await fse.remove(path.resolve('spec/support'));
-    });
-
     it('returns an array of spec files', async () => {
-      const {stdout} = await childProcess.exec(
-        `find '${path.resolve('spec')}' -type f`);
-      const expectedFiles = stdout.trim().split('\n');
-
-      const actualFiles = await specFiles();
-
-      expect(actualFiles.sort()).toEqual(expectedFiles.sort());
-    });
-
-    it('respects jasmine.json configuration', async () => {
-      await fse.writeJson(path.resolve('spec/support/jasmine.json'), {
-        spec_dir: 'spec',
-        spec_files: [
-          'lib/reflect_*.js'
-        ]
+      const project = new Project();
+      await project.write({
+        'spec/support/jasmine.json': {
+          spec_dir: 'spec',
+          spec_files: ['**/*_spec.js']
+        },
+        'spec/index_spec.js': '',
+        'spec/lib/foo_spec.js': ''
       });
+      const result = await project.run(`
+        import {specFiles} from 'jasmine-es6/lib/reflect';
+        async function run() {
+          process.stdout.write(JSON.stringify(await specFiles()));
+        }
+        run();
+      `);
 
-      const expectedFiles = [
-        path.resolve('spec/lib/reflect_spec.js')
-      ];
-
-      const actualFiles = await specFiles();
-
-      expect(actualFiles.sort()).toEqual(expectedFiles.sort());
+      expect(JSON.parse(result)).toEqual([
+        project.path('spec/index_spec.js'),
+        project.path('spec/lib/foo_spec.js')
+      ]);
     });
   });
 });
